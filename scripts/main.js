0900692ed8e40ccd8f7c3c31b10dbd9f929fc24b
Krättli Noé <113976898+HackerBurscht@users.noreply.github.com>
2025-04-11 15:20:18 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,21 @@
-//Infinity Slider Gallery
+// Infinity Slider Gallery
 const gallery = document.querySelector('.gallery');
 gallery.innerHTML += gallery.innerHTML;
 
-//Footer animation
-  document.addEventListener('DOMContentLoaded', () => {
-    // Elemente, die animiert werden sollen:
-    const targets = document.querySelectorAll('.footer_logo_container, .footer_small_links');
+// Footer animation
+document.addEventListener('DOMContentLoaded', () => {
+  // Elemente, die animiert werden sollen:
+  const targets = document.querySelectorAll('.footer_logo_container, .footer_small_links');
 
-    const observer = new IntersectionObserver((entries, observer) => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.classList.add('animate');
-          // Unobserve, wenn du die Animation nur einmal starten möchtest:
-          observer.unobserve(entry.target);
-        }
-      });
-    }, { threshold: 0.2 }); // 20% Sichtbarkeit als Trigger
+  const observer = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add('animate');
+        // Unobserve, wenn du die Animation nur einmal starten möchtest:
+        observer.unobserve(entry.target);
+      }
+    });
+  }, { threshold: 0.2 }); // 20% Sichtbarkeit als Trigger
 
-    targets.forEach(target => observer.observe(target));
-  });
-
+  targets.forEach(target => observer.observe(target));
+});
