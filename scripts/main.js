9b495b450ef708e4a751b5ed07ef81202acdac5f
Krättli Noé <113976898+HackerBurscht@users.noreply.github.com>
2025-04-11 14:15:13 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
 //Infinity Slider Gallery
 const gallery = document.querySelector('.gallery');
 gallery.innerHTML += gallery.innerHTML;
+
+//Footer animation
+// Selektiere den Bereich, in dem die animierte Linie enthalten ist
+const footerSmallLinks = document.querySelector('.footer_small_links');
+
+// Erstelle den Intersection Observer
+const observer = new IntersectionObserver(
+  (entries, observer) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        // Klasse hinzufügen, damit das ::after-Element animiert
+        footerSmallLinks.classList.add('animate');
+        // Falls die Animation nur einmal erfolgen soll:
+        observer.disconnect();
+      }
+    });
+  },
+  { threshold: 0.5 } // z. B. 50 % sichtbar
+);
+
+// Beobachte den ausgewählten Container
+observer.observe(footerSmallLinks);
+
