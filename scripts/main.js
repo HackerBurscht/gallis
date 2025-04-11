329a4540f3e8a012b8ac534f6db198871347a05f
Krättli Noé <113976898+HackerBurscht@users.noreply.github.com>
2025-04-11 14:46:39 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -23,4 +23,21 @@ const observer = new IntersectionObserver(
 
 // Beobachte den ausgewählten Container
 observer.observe(footerSmallLinks);
+  // Selektiere den Logo-Container
+  const logoContainer = document.querySelector('.footer_logo_container');
+
+  // Intersection Observer erstellen, der auslöst, wenn der Container zur Hälfte sichtbar ist
+  const observerLogo = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          logoContainer.classList.add('animate');
+          observer.disconnect(); // Optional: nur einmal auslösen
+        }
+      });
+    },
+    { threshold: 0.5 } // 50 % Sichtbarkeit
+  );
+
+  observerLogo.observe(logoContainer);
 
