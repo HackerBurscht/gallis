1c50da8496ee0fde7eb021289c3887705344fae0
Krättli Noé <113976898+HackerBurscht@users.noreply.github.com>
2025-04-25 07:07:31 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-*document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('.aboutus_img_container');
   const image = container ? container.querySelector('.aboutus_img') : null;
 
